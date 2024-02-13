c2cf7226c08f1caa1c4dab5ff3abc2a8f9689c19
saiahs_git <saiahs.ent@gmail.com>
2024-02-13 20:16:47 +0200

    checked from home

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: oadewumi <oadewumi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 14:58:07 by oadewumi          #+#    #+#             */
-/*   Updated: 2024/02/12 16:21:32 by oadewumi         ###   ########.fr       */
+/*   Updated: 2024/02/13 20:07:38 by oadewumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,6 +89,7 @@ int	main(void)
 {
 	int		fd;
 	char	*str;
+	
 
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
@@ -99,12 +100,59 @@ int	main(void)
 
 	str = get_next_line(fd);
 
+
 	while (str)
 	{
 		printf("GNL:%s\n", str);
 		free(str);
 		str = get_next_line(fd);
-
 	}
 	return (0);
 }
+
+/*
+#include "get_next_line.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	char *line;
+	int fd;
+	
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s file1 file2 ...\n", argv[0]);
+		return (1);
+		}
+	for (int i = 1; i < argc; i++)
+	{
+		fd = open(argv[i], O_RDONLY);
+		if (fd == -1)
+		{
+			perror("Error opening file");
+			continue; // Skip to the next file
+		}
+		printf("FILE: %s\n", argv[i]);
+		while (1)
+		{
+			line = get_next_line(fd);
+			if (line)
+			printf("%s", line);
+				else
+			{
+				printf("\nEOL: %s\n", argv[i]);
+				break;
+				}
+			free(line);
+		}
+		close(fd);
+	}
+	if (system("leaks a.out > /dev/null"))
+		system("leaks a.out");
+	return (0);
+}
+*/
